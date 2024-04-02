f3eda154a37459672fac0e54f1de8859b2c60254
Matija Kočevar <matija.kocev@gmail.com>
2024-04-02 08:59:10 +0200

    Fixed shadcn not working

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function HomePage() {
     return (
         <>
-            <div className="h-screen">
-                <UserButton afterSignOutUrl="/" />
-            </div>
+            <div>Home</div>
         </>
     );
 }
