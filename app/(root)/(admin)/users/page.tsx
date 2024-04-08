ad691060037c661cabf3b49cc7b8365e35e19b1e
Matija Kočevar <matija.kocev@gmail.com>
2024-04-08 14:43:56 +0200

    Move components directly into page

--- app/(root)/(admin)/users/page.tsx
+++ app/(root)/(admin)/users/page.tsx
@@ -1,9 +1,121 @@
-import React from "react";
-import { UserDataTable } from "../../../../components/users/UserDataTable";
-import { userTableColumns } from "../../../../constants/user-table-columns";
+"use client";
 
-const UsersPage = async () => {
-    return <UserDataTable columns={userTableColumns} />;
+import {
+    ColumnFiltersState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import { useRouter } from "next/navigation";
+import { userTableColumns } from "../../../../constants/user-table-columns";
+import React, { useEffect, useState } from "react";
+import { User } from "@clerk/nextjs/server";
+import { Input } from "../../../../components/ui/input";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../../../../components/ui/table";
+
+const UsersPage = () => {
+    const [data, setData] = useState<User[]>([]);
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+    const table = useReactTable({
+        data,
+        columns: userTableColumns,
+        getCoreRowModel: getCoreRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        state: {
+            columnFilters,
+        },
+    });
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch("/users/api/get-users");
+
+                if (!response.ok) {
+                    throw new Error("Failed to fetch users");
+                }
+
+                const users = await response.json();
+                setData(users);
+                setLoading(false);
+            } catch (error) {
+                console.error("Failed to fetch users", error);
+                setLoading(false);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    useEffect(() => {
+        console.log("datache changed", data);
+    }, [data]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Filter by name..."
+                    value={(table.getColumn("firstName")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) => table.getColumn("firstName")?.setFilterValue(event.target.value)}
+                    className="max-w-sm"
+                />
+            </div>
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(header.column.columnDef.header, header.getContext())}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    className="hover:bg-muted-1 cursor-pointer"
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"}
+                                    onClick={() => router.push(`/users/update/${row.original.id}`)}
+                                >
+                                    {row.getVisibleCells().map((cell) => {
+                                        return (
+                                            <TableCell key={cell.id}>
+                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                            </TableCell>
+                                        );
+                                    })}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={userTableColumns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+        </>
+    );
 };
 
 export default UsersPage;
