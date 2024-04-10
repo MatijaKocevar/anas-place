8575d2933bc52e90b889c7eaf098ab0f29a7c96d
Matija Kočevar <matija.kocev@gmail.com>
2024-04-11 00:33:35 +0200

    Add pagination and css fixes

--- app/(root)/(admin)/users/page.tsx
+++ app/(root)/(admin)/users/page.tsx
@@ -21,6 +21,7 @@ import {
     TableRow,
 } from "../../../../components/ui/table";
 import useUsers from "../../../../hooks/useUsers";
+import { Button } from "../../../../components/ui/button";
 
 const UsersPage = () => {
     const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
@@ -32,8 +33,14 @@ const UsersPage = () => {
         getCoreRowModel: getCoreRowModel(),
         onColumnFiltersChange: setColumnFilters,
         getFilteredRowModel: getFilteredRowModel(),
-        // getPaginationRowModel: getPaginationRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
         state: {
+            columnFilters,
+        },
+        initialState: {
+            pagination: {
+                pageSize: 15,
+            },
             columnFilters,
         },
     });
@@ -41,72 +48,102 @@ const UsersPage = () => {
     if (loading) return <div>Loading...</div>;
 
     return (
-        <div className="w-full h-screen-100 max-h-screen-100 overflow-y-auto">
-            <div className="flex items-center pb-5">
-                <Input
-                    placeholder="Filter by name..."
-                    value={(table.getColumn("firstName")?.getFilterValue() as string) ?? ""}
-                    onChange={(event) =>
-                        table.getColumn("firstName")?.setFilterValue(event.target.value)
-                    }
-                    className="max-w-sm"
-                />
+        <div className="flex justify-around flex-col h-screen-9 max-h-screen-9 overflow-hidden">
+            <div className="flex justify-between items-center py-2">
+                <div className="">
+                    <Input
+                        placeholder="Filter by name..."
+                        value={(table.getColumn("firstName")?.getFilterValue() as string) ?? ""}
+                        onChange={(event) =>
+                            table.getColumn("firstName")?.setFilterValue(event.target.value)
+                        }
+                        className="max-w-sm"
+                    />
+                </div>
+                <div className="flex flex-row">
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        Previous
+                    </Button>
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        Next
+                    </Button>
+                </div>
             </div>
-
-            <div className="rounded-md border">
-                <Table>
-                    <TableHeader>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead key={header.id}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                      header.column.columnDef.header,
-                                                      header.getContext()
-                                                  )}
-                                        </TableHead>
-                                    );
-                                })}
-                            </TableRow>
-                        ))}
-                    </TableHeader>
-
-                    <TableBody className="overflow-y-auto max-h-full">
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    className="hover:bg-muted-1 cursor-pointer"
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                    onClick={() => router.push(`/users/update/${row.original.id}`)}
-                                >
-                                    {row.getVisibleCells().map((cell) => {
+            <div className="rounded-md border overflow-auto">
+                <div className="sticky top-0 z-50">
+                    <Table>
+                        <TableHeader>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <TableRow key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => {
                                         return (
-                                            <TableCell key={cell.id}>
-                                                {flexRender(
-                                                    cell.column.columnDef.cell,
-                                                    cell.getContext()
-                                                )}
-                                            </TableCell>
+                                            <TableHead
+                                                className="bg-white w-[33%] px-2"
+                                                key={header.id}
+                                            >
+                                                {header.isPlaceholder
+                                                    ? null
+                                                    : flexRender(
+                                                          header.column.columnDef.header,
+                                                          header.getContext()
+                                                      )}
+                                            </TableHead>
                                         );
                                     })}
                                 </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell
-                                    colSpan={userTableColumns.length}
-                                    className="h-24 text-center"
-                                >
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
+                            ))}
+                        </TableHeader>
+                    </Table>
+                </div>
+
+                <div className="overflow-auto">
+                    <Table>
+                        <TableBody>
+                            {table.getRowModel().rows?.length ? (
+                                table.getRowModel().rows.map((row) => (
+                                    <TableRow
+                                        className="hover:bg-muted-1 cursor-pointer"
+                                        key={row.id}
+                                        data-state={row.getIsSelected() && "selected"}
+                                        onClick={() =>
+                                            router.push(`/users/update/${row.original.id}`)
+                                        }
+                                    >
+                                        {row.getVisibleCells().map((cell) => {
+                                            return (
+                                                <TableCell className="w-[33%] px-2" key={cell.id}>
+                                                    {flexRender(
+                                                        cell.column.columnDef.cell,
+                                                        cell.getContext()
+                                                    )}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell
+                                        colSpan={userTableColumns.length}
+                                        className="h-24 text-center"
+                                    >
+                                        No results.
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </div>
             </div>
         </div>
     );
