3ee4b1cdbba5ad998247807598522aef3f52d2a9
Matija Kočevar <matija.kocev@gmail.com>
2024-04-12 01:33:37 +0200

    Update clerckClient to clerkApi

--- app/(root)/(admin)/users/api/get-user/[userId]/route.ts
+++ app/(root)/(admin)/users/api/get-user/[userId]/route.ts
@@ -1,7 +1,7 @@
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkApi } from "@clerk/nextjs/edge-middlewarefiles";
 
 export async function GET(req: Request, { params }: { params: { userId: string } }) {
-    const user = await clerkClient.users?.getUser(params.userId);
+    const user = await clerkApi.users?.getUser(params.userId);
 
     if (!user) return new Response(null, { status: 404 });
 
