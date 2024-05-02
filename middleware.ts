845e7f993ba1e23f0dd3da9184ea57f501c55817
Matija Kočevar <matija.kocev@gmail.com>
2024-05-02 23:52:24 +0200

    Build fix

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,14 @@ import { roles } from "./constants/roles";
 const activeOrgId = process.env.CLERK_ACTIVE_ORGANIZATION_ID;
 
 export default authMiddleware({
-    publicRoutes: ["/", "/sign-in", "/sign-up", "/gallery", "/services"],
+    publicRoutes: ["/", "/sign-in", "/sign-up", "/gallery", "/api/gallery", "/services"],
     afterAuth(auth, req) {
         if (!auth.userId && !auth.isPublicRoute) {
             return redirectToSignIn({ returnBackUrl: req.url });
         }
 
         if (auth.userId && activeOrgId) {
-            const protectedPaths = ["/users", "/receipts", "/bookings"];
+            const protectedPaths = ["/users", "/api/users", "/receipts", "/bookings"];
             const isAccessingProtectedRoute = protectedPaths.some((protectedPath) =>
                 req.nextUrl.pathname.startsWith(protectedPath)
             );
