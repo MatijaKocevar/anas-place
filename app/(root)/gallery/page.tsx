92c9db6c4d3d93454d871df3122baee5e21622cf
Matija Kočevar <matija.kocev@gmail.com>
2024-05-02 23:41:49 +0200

    Create separate Gallery component

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -1,46 +1,9 @@
-"use client";
-
-import Image from "next/image";
-import useInstagramPosts from "../../../hooks/useInstagramPosts";
-import Spinner from "../../../components/ui/Spinner";
+import Gallery from "../../_components/Gallery";
 
 const GalleryPage = () => {
-    const { instagramPosts, loading } = useInstagramPosts();
-
-    if (loading) {
-        return (
-            <div className="flex justify-start flex-col h-screen-9 max-h-screen-9 overflow-hidden">
-                <Spinner />
-            </div>
-        );
-    }
-
     return (
         <div className="w-full h-screen-9 max-h-screen-9 overflow-y-auto">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4">
-                {instagramPosts.map((post) => (
-                    <div key={post.id} className="bg-white shadow rounded overflow-hidden">
-                        <Image
-                            width={500}
-                            height={500}
-                            src={post.media_url}
-                            alt={post.caption}
-                            className="w-full h-auto"
-                        />
-                        <div className="p-4">
-                            <p className="text-sm">{post.caption}</p>
-                            <a
-                                href={post.permalink}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-blue-500 hover:text-blue-600 transition duration-300 ease-in-out"
-                            >
-                                View on Instagram
-                            </a>
-                        </div>
-                    </div>
-                ))}
-            </div>
+            <Gallery />
         </div>
     );
 };
