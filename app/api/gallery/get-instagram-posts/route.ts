eed7e60600fced4b7466e881034ba92d04556b08
Matija Kočevar <matija.kocev@gmail.com>
2024-05-02 23:24:40 +0200

    Add gallery page

---
+++ app/api/gallery/get-instagram-posts/route.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const INSTAGRAM_API_URL = "https://graph.instagram.com";
+const ACCESS_TOKEN =
+    "IGQWRPb2ZAVRG1CS0huVXNnRW1sdy1mbEtCSXMxQlpvTlJnanNZAQmV1YWktR2tsMkVEQ0RDWk1VLVdwdS1lTU1iZAk1XVjZAoc3VmM2l1bTJCMnR0NFFNbVdScmprcEVheHpNTDFPdUJ2NkJHUmlhNWVISkVaWENnNFkZD";
+
+export type InstagramPost = {
+    id: string;
+    caption: string;
+    media_type: string;
+    media_url: string;
+    permalink: string;
+};
+
+export async function GET(request: Request) {
+    try {
+        const result = await axios.get(`${INSTAGRAM_API_URL}/me/media`, {
+            params: {
+                fields: "id,caption,media_type,media_url,permalink",
+                access_token: ACCESS_TOKEN,
+            },
+        });
+
+        request.headers.set("Cache-Control", "public, max-age=0, s-maxage=0");
+
+        const posts = result.data;
+
+        return Response.json(posts.data);
+    } catch (error) {
+        console.error("Error fetching Instagram posts:", error);
+        return null;
+    }
+}
