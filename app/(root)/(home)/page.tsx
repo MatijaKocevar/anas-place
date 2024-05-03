7f7e63c1f8a024ed4ef948072eb0a95bcdbd6952
Matija Kočevar <matija.kocev@gmail.com>
2024-05-03 04:04:19 +0200

    Add temp content to home and services

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,21 @@
-import { loremIpsumBig } from "../../../constants/loremIpsumBig";
+import Image from "next/image";
 
 const HomePage = () => {
     return (
-        <div className="w-full h-screen-9 max-h-screen-9 overflow-y-auto">
-            <p className="text-black">{loremIpsumBig}</p>
+        <div className="w-full h-screen-9 max-h-screen-9 overflow-y-auto flex flex-col justify-center text-center">
+            <h1 className="text-3xl font-bold text-black mb-4">Welcome to Ana&apos;s place!</h1>
+            <p className="text-black mb-4">
+                Experience the art of nail design with Ana&apos;s place. Our salon provides a
+                relaxing atmosphere and premium services to ensure you leave feeling pampered and
+                polished.
+            </p>
+            <p className="text-black">
+                Our website is currently under construction, but soon you will be able to book
+                appointments online with ease!
+            </p>
+            <div className="w-full flex justify-center pt-4">
+                <Image src="/icons/logo.svg" width={100} height={100} alt="logo" />
+            </div>
         </div>
     );
 };
