708932bb34d56a8191b3ddc10fbd8c47b3361e6f
Matija Kočevar <matija.kocev@gmail.com>
2024-05-08 14:07:39 +0200

    Gallery page now infinite loading posts

--- hooks/useInstagramPosts.ts
+++ hooks/useInstagramPosts.ts
@@ -4,28 +4,42 @@ import { InstagramPost } from "../app/api/gallery/get-instagram-posts/route";
 const useInstagramPosts = () => {
     const [instagramPosts, setInstagramPosts] = useState<InstagramPost[]>([]);
     const [loading, setLoading] = useState(true);
+    const [nextPageUrl, setNextPageUrl] = useState<string | null>(null);
+    const [isFetchingMore, setIsFetchingMore] = useState(false);
+
+    const fetchInstagramPosts = async (url?: string) => {
+        setIsFetchingMore(true);
+        try {
+            const response = await fetch(
+                url
+                    ? `/api/gallery/get-instagram-posts?nextPageUrl=${encodeURIComponent(url)}`
+                    : "/api/gallery/get-instagram-posts"
+            );
+
+            if (!response.ok) throw new Error("Failed to fetch Instagram posts");
+
+            const postsData = await response.json();
+            setInstagramPosts((prev) => [...prev, ...postsData.data]);
+            setNextPageUrl(postsData.paging?.next || null);
+        } catch (error) {
+            console.error("Failed to fetch Instagram posts", error);
+        } finally {
+            setLoading(false);
+            setIsFetchingMore(false);
+        }
+    };
 
     useEffect(() => {
-        const fetchInstagramPosts = async () => {
-            setLoading(true);
-            try {
-                const response = await fetch("/api/gallery/get-instagram-posts");
-
-                if (!response.ok) throw new Error("Failed to fetch instagram posts");
-
-                const posts = (await response.json()) as InstagramPost[];
-                setInstagramPosts(posts);
-            } catch (error) {
-                console.error("Failed to fetch instagram posts", error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchInstagramPosts();
+        fetchInstagramPosts(); // Initial fetch
     }, []);
 
-    return { instagramPosts, loading };
+    const loadMore = () => {
+        if (nextPageUrl) {
+            fetchInstagramPosts(nextPageUrl);
+        }
+    };
+
+    return { instagramPosts, loading, isFetchingMore, loadMore };
 };
 
 export default useInstagramPosts;
