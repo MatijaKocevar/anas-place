5d0451bbda0121c03ef6c3b1de7876eac80e49d1
Matija Kočevar <matija.kocev@gmail.com>
2024-05-11 23:48:32 +0200

    Hide scrollbar

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -2,7 +2,7 @@ import Gallery from "../../_components/Gallery";
 
 const GalleryPage = () => {
     return (
-        <div className="w-full h-screen-9 max-h-screen-9 overflow-y-auto">
+        <div className="w-full h-screen-9 max-h-screen-9 overflow-y-auto hidden-scrollbar">
             <Gallery />
         </div>
     );
