56b0bf500fb2ff1779a0cdffc8113f109e8d661b
Matija Kočevar <matija.kocev@gmail.com>
2024-05-12 21:32:57 +0200

    build fix

--- app/api/cron/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { refreshInstagramToken } from "../../../data/instagram-service";
-
-export async function GET() {
-    const result = await refreshInstagramToken();
-    const data = await result.json();
-
-    return new NextResponse(JSON.stringify({ datetime: data.datetime }));
-}
