5b82fb4602f78c94a93f03029ac675c0e4d250da
Matija Kočevar <matija.kocev@gmail.com>
2024-05-13 13:03:38 +0200

    Test gallery cache

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -1,6 +1,6 @@
 import Gallery from "../../_components/Gallery";
 
-const GalleryPage = async () => {
+const GalleryPage = () => {
     return (
         <div className="w-full h-screen-9 max-h-screen-9 overflow-y-auto hidden-scrollbar">
             <Gallery />
