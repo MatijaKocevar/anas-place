4be5cfc6c1e7e0aa9fede15e604a790e4ae5a943
Matija Kočevar <matija.kocev@gmail.com>
2024-05-13 13:03:38 +0200

    Test gallery cache

--- app/_components/Gallery.tsx
+++ app/_components/Gallery.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useEffect, useRef, useState } from "react";
 import BlurImage from "../../components/BlurImage";
 import Spinner from "../../components/ui/Spinner";
@@ -13,67 +11,69 @@ export type InstagramPost = {
     permalink: string;
 };
 
-const Gallery = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [instagramPosts, setInstagramPosts] = useState<InstagramPost[]>([]);
-    const [nextPageUrl, setNextPageUrl] = useState<string | null>(null);
+const Gallery = async () => {
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [instagramPosts, setInstagramPosts] = useState<InstagramPost[]>([]);
+    // const [nextPageUrl, setNextPageUrl] = useState<string | null>(null);
 
-    const observerRef = useRef<HTMLDivElement | null>(null);
+    // const observerRef = useRef<HTMLDivElement | null>(null);
 
-    const fetchInitialInstagramPosts = async () => {
-        try {
-            const response = await getInitialInstagramPosts();
+    // const fetchInitialInstagramPosts = async () => {
+    //     try {
+    //         const response = await getInitialInstagramPosts();
 
-            setNextPageUrl(response.paging.next);
-            setInstagramPosts(response.data);
-        } catch (error) {
-            console.error("Failed to fetch Instagram posts", error);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+    //         setNextPageUrl(response.paging.next);
+    //         setInstagramPosts(response.data);
+    //     } catch (error) {
+    //         console.error("Failed to fetch Instagram posts", error);
+    //     } finally {
+    //         setIsLoading(false);
+    //     }
+    // };
 
-    const fetchMoreInstagramPosts = async (url: string) => {
-        try {
-            const response = await getMoreInstagramPosts(url);
+    // const fetchMoreInstagramPosts = async (url: string) => {
+    //     try {
+    //         const response = await getMoreInstagramPosts(url);
 
-            setNextPageUrl(response.paging.next);
-            setInstagramPosts((prevPosts) => {
-                const existingPostIds = new Set(prevPosts.map((p) => p.id));
-                const filteredNewPosts = response.data.filter(
-                    (post) => !existingPostIds.has(post.id)
-                );
-                return [...prevPosts, ...filteredNewPosts];
-            });
-        } catch (error) {
-            console.error("Failed to fetch more Instagram posts", error);
-        }
-    };
+    //         setNextPageUrl(response.paging.next);
+    //         setInstagramPosts((prevPosts) => {
+    //             const existingPostIds = new Set(prevPosts.map((p) => p.id));
+    //             const filteredNewPosts = response.data.filter(
+    //                 (post) => !existingPostIds.has(post.id)
+    //             );
+    //             return [...prevPosts, ...filteredNewPosts];
+    //         });
+    //     } catch (error) {
+    //         console.error("Failed to fetch more Instagram posts", error);
+    //     }
+    // };
 
-    useEffect(() => {
-        fetchInitialInstagramPosts();
-    }, []);
+    // useEffect(() => {
+    //     fetchInitialInstagramPosts();
+    // }, []);
 
-    useEffect(() => {
-        if (observerRef.current) {
-            const observer = new IntersectionObserver(
-                (entries) => {
-                    if (entries[0].isIntersecting && !isLoading && nextPageUrl) {
-                        fetchMoreInstagramPosts(nextPageUrl);
-                    }
-                },
-                { threshold: 0.1 }
-            );
+    // useEffect(() => {
+    //     if (observerRef.current) {
+    //         const observer = new IntersectionObserver(
+    //             (entries) => {
+    //                 if (entries[0].isIntersecting && !isLoading && nextPageUrl) {
+    //                     fetchMoreInstagramPosts(nextPageUrl);
+    //                 }
+    //             },
+    //             { threshold: 0.1 }
+    //         );
 
-            observer.observe(observerRef.current);
-            return () => observer.disconnect();
-        }
-    }, [nextPageUrl, isLoading]);
+    //         observer.observe(observerRef.current);
+    //         return () => observer.disconnect();
+    //     }
+    // }, [nextPageUrl, isLoading]);
+
+    const response = await getInitialInstagramPosts();
 
     return (
         <>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4">
-                {instagramPosts.map((post, index) => (
+                {response.data.map((post, index) => (
                     <div key={post.id} className="bg-white shadow rounded-2xl overflow-hidden">
                         <BlurImage
                             image={{
@@ -88,9 +88,9 @@ const Gallery = () => {
                     </div>
                 ))}
             </div>
-            <div ref={observerRef} className="text-center py-8">
+            {/* <div ref={observerRef} className="text-center py-8">
                 {isLoading && <Spinner />}
-            </div>
+            </div> */}
         </>
     );
 };
