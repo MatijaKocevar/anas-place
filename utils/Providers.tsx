a0fd8ac3b61ad56e67e1e6f1c3370b510be6d575
Matija Kočevar <matija.kocev@gmail.com>
2024-06-02 11:12:03 +0200

    Update providers with correct query client

--- utils/Providers.tsx
+++ utils/Providers.tsx
@@ -1,19 +1,16 @@
 "use client";
 import React, { ReactNode } from "react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ClerkProvider } from "@clerk/nextjs";
+import { getQueryClient } from "./get-query-client";
 
 function Providers({ children }: { children: ReactNode }) {
-    const client = new QueryClient();
+    const client = getQueryClient();
 
     return (
         <>
-            <ClerkProvider
-                appearance={{
-                    variables: {},
-                }}
-            >
+            <ClerkProvider>
                 <QueryClientProvider client={client}>
                     {children}
                     <ReactQueryDevtools />
