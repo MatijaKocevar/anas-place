6b0fa2683ed804841d5fcf24d52addb49dbc5f58
Matija Kočevar <matija.kocev@gmail.com>
2024-06-04 18:20:03 +0200

    Build fix

--- app/(root)/(admin)/bookings/update/[bookingId]/page.tsx
+++ app/(root)/(admin)/bookings/update/[bookingId]/page.tsx
@@ -1,9 +1,7 @@
-import { loremIpsumBig } from "../../../../../../constants/loremIpsumBig";
-
 export default function UpdateBookingPage() {
     return (
-        <div className="w-full h-screen-9 max-h-screen-9 overflow-y-auto">
-            <p className="text-black">{loremIpsumBig}</p>
+        <div className="w-full h-screen-9 max-h-screen-9 overflow-y-auto flex justify-center">
+            <p className="flex text-black items-center">V IZDELAVI</p>
         </div>
     );
 }
